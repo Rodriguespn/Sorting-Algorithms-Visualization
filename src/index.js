85417ba830f01cb7ee93cd5861c8fed3ae7c0f10
rodriguespn <pedroneverodrigues@gmail.com>
2020-09-14 13:43:13 +0100

    Add insertion sort

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // Enum
 const algorithms = {
     BUBBLE: 1,
-    SELECTION: 2
+    SELECTION: 2,
+    INSERTION: 3
 }
     
 const canvas = document.querySelector(".data-container");
@@ -10,17 +11,18 @@ let sortingAlgo = algorithms.BUBBLE;
 let active = false;
 const delay = 70;
 const initColour = "#3aafff";
-const selectedColour = "#13CE66";
-const doneColour = "#d849ff";
+const selectedColour = "#d849ff";
+const doneColour = "#13CE66";
 
+// Clears the blocks from the canvas
 function clearCanvas() {
     while(canvas.firstChild) { 
         canvas.removeChild(canvas.firstChild); 
     } 
 }
 
+// generates new blocks on the canvas
 function generateBlocks(num = 30) {
-
     clearCanvas();
 
     num = canvas.clientWidth / 30 - 1;
@@ -45,15 +47,32 @@ function generateBlocks(num = 30) {
     }
 }
 
+// changes the selected algorithm code
 function changeAlgorithm(code) {
     if (sortingAlgo != code) {
         generateBlocks();
         sortingAlgo = code;
+        active = false;
+        const header = document.querySelector(".algorithm-header");
+        switch (code) {
+            case algorithms.BUBBLE:
+                header.innerText = "Bubble Sort";
+                break;
+            case algorithms.SELECTION:
+                header.innerText = "Selection Sort";
+                break;
+            case algorithms.INSERTION:
+                header.innerText = "Insertion Sort";
+                break;
+            default:
+                break;
+        }
     }
     console.log(sortingAlgo);
 }
 
-function swap(el1, el2) {
+// swap el1 with el2 but only visualy. Does not change the order on the array
+function swapVisual(el1, el2) {
     return new Promise(resolve => {
         const style1 = window.getComputedStyle(el1);
         const style2 = window.getComputedStyle(el2);
@@ -74,8 +93,16 @@ function swap(el1, el2) {
     });
 }
 
+// swap the element in the position i with the element on the position j and returns the modified array
+async function swap(array, i, j) {
+    await swapVisual(array[i], array[j]);
+    temp = array[i];
+    array.splice(i, 1, array[j]);
+    array.splice(j, 1, temp);
+    return array;
+}
+
 async function bubbleSort() {
-    
     if (delay && typeof delay !== "number") {
         alert("sort: First argument must be a typeof Number");
         return;
@@ -94,30 +121,25 @@ async function bubbleSort() {
             );
 
             blocks[j].style.backgroundColour = selectedColour;
-            blocks[j + 1].style.backgroundColour = selectedColour;
+            blocks[j+1].style.backgroundColour = selectedColour;
 
             const value1 = Number(blocks[j].childNodes[0].innerHTML);
             const value2 = Number(blocks[j + 1].childNodes[0].innerHTML);
 
             if (value1 > value2) {
-                await swap(blocks[j], blocks[j + 1]);
-                temp = blocks[j];
-                blocks.splice(j, 1, blocks[j+1]);
-                blocks.splice(j+1, 1, temp);
-                //blocks = document.querySelectorAll(".block");
+                blocks = await swap(blocks, j, j+1);
             }
 
-            blocks[j].style.backgroundColour = doneColour;
-            blocks[j + 1].style.backgroundColour = doneColour;
+            blocks[j].style.backgroundColour = initColour;
+            blocks[j + 1].style.backgroundColour = initColour;
         }
 
-        blocks[blocks.length - i - 1].style.backgroundColour = initColour;
+        blocks[blocks.length - i - 1].style.backgroundColour = doneColour;
     }
     blocks[0].style.backgroundColour = doneColour;
 }
 
 async function selectionSort() {
-
     if (delay && typeof delay !== "number") {
         alert("sort: First argument must be a typeof Number");
         return;
@@ -153,23 +175,56 @@ async function selectionSort() {
             //blocks[j + 1].style.backgroundColour = doneColour;
         }
 
-        for (let k = 0; k < blocks.length; k++)
-            console.log(Number(blocks[k].childNodes[0].innerHTML));
-        console.log("\n");
-        await swap(blocks[min], blocks[i]);
-        //blocks = document.querySelectorAll(".block");
-        
-        let temp = blocks[min];
-        blocks.splice(min, 1, blocks[i]);
-        blocks.splice(i, 1, temp);
+        blocks = await swap(blocks, min, i);
 
-        for (let k = 0; k < blocks.length; k++)
-            console.log(Number(blocks[k].childNodes[0].innerHTML));
         blocks[i].style.backgroundColour = doneColour;
             
         //blocks[blocks.length - i - 1].style.backgroundColour = initCoulour;
     }
     //blocks[0].style.backgroundColour = doneColour;
+}
+
+async function insertionSort() {
+    if (delay && typeof delay !== "number") {
+        alert("sort: First argument must be a typeof Number");
+        return;
+    }
+    let htmlElements = document.querySelectorAll(".block");
+    let blocks = new Array();
+    for (let i = 0; i < htmlElements.length; i++) {
+        blocks.push(htmlElements[i]);
+    }
+    for (let i = 1; i < blocks.length; i += 1) {
+        /*console.log('\n');
+        for (let k=0; k < blocks.length; k++) {
+            console.log(Number(blocks[k].childNodes[0].innerHTML));
+        }
+        console.log('\n');*/
+        let valueToInsert = Number(blocks[i].childNodes[0].innerHTML);
+        let j=i-1;
+        while (j >= 0 && Number(blocks[j].childNodes[0].innerHTML) > valueToInsert) {
+            await new Promise(resolve =>
+                setTimeout(() => {
+                    resolve();
+                }, delay)
+            );
+
+            /*blocks[j].style.backgroundColour = selectedColour;
+            blocks[j + 1].style.backgroundColour = selectedColour;*/
+
+            blocks = await swap(blocks, j, j+1);
+
+            blocks[j].style.backgroundColour = doneColour;
+            blocks[j + 1].style.backgroundColour = doneColour;
+
+            j -= 1;
+        }
+
+        console.log(j+1);
+
+        //blocks[blocks.length - i - 1].style.backgroundColour = initColour;
+    }
+    blocks[0].style.backgroundColour = doneColour;
 }
 
 function sort() {
@@ -184,6 +239,11 @@ function sort() {
             case algorithms.SELECTION:
                 selectionSort();
                 console.log("Selection sort");
+                break;
+
+            case algorithms.INSERTION:
+                insertionSort();
+                console.log("Insertion sort");
                 break;
             
             default:
